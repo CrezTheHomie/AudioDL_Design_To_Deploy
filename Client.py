c25f9cbec115efbe4ecb50107fa8882437d3912b
CrezTheHomie <crezyte@gmail.com>
2023-01-12 11:21:14 -0800

    Client now properly passes file to service.

--- Client.py
+++ Client.py
@@ -1,56 +1,33 @@
 import requests
-import json
-from bentoml.io import File
-import librosa
-import numpy as np
 
 CLASSIFY_FILE_URL = "http://127.0.0.1:3000/classify_file"
 CLASSIFY_MFCCs_URL = "http://127.0.0.1:3000/classify_MFCCs"
 TEST_AUDIO_FILE_PATH = "Test\\00f0204f_nohash_0.wav"
+RAND_PDF_PATH = "Test\\ml-canvas.pdf"
 
 
 def make_request_to_bento_service(
     service_url: str, input_file, header_dict: dict
 ) -> str:
+
+    # with open(TEST_AUDIO_FILE_PATH, 'rb') as f:
+    #     audio_data = f.read()
+
     response = requests.post(
-        service_url,
-        data=input_file,
-        headers=header_dict
-    )
+        service_url, headers=header_dict, data=input_file)
     return response.text
-
-def preprocess_file(input_file):
-    signal, sr = librosa.load(input_file)
-
-    # ensure consistency of audio file length
-    if len(signal) > 22050:
-            signal = signal[:22050]
-
-    # extract MFCCs
-    MFCCs = librosa.feature.mfcc(
-        y=signal, n_mfcc=13, n_fft=2048, hop_length=512)
-    MFCCs = MFCCs.T
-    MFCCs = MFCCs[np.newaxis, ..., np.newaxis]
-    return MFCCs
 
 if __name__ == "__main__":
     
-    _mappings = [
-        "bed", "bird", "cat", "dog", "down", "eight",
-        "five", "four", "go", "happy", "house",
-        "left", "marvin", "nine", "no", "off", "on",
-        "one", "right", "seven", "sheila", "six", "stop",
-        "three", "tree", "two", "up", "wow", "yes", "zero"
-    ]
-    
-    audio_file = open(TEST_AUDIO_FILE_PATH, "rb")
-    values = {"file": (TEST_AUDIO_FILE_PATH, audio_file, "audio/wav")}
-    MFCCs = preprocess_file(audio_file)
+    with open(TEST_AUDIO_FILE_PATH, 'rb') as f:
+        audio_data = f.read()
 
-    headers_dict = {"content-type": "application/json"}
-    serialized_input_data = json.dumps(MFCCs.tolist())
+    headers_dict = {
+        'accept': 'application/json',
+        'Content-Type': 'application/octet-stream',
+    }
 
     response = make_request_to_bento_service(
-        service_url=CLASSIFY_MFCCs_URL, input_file=serialized_input_data, header_dict=headers_dict)
+        service_url=CLASSIFY_FILE_URL, input_file=audio_data, header_dict=headers_dict)
     
     print(f"Response was: {response}")
