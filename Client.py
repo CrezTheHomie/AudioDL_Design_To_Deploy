50eeabd128f118624c2cfdf5e4e24450e2b6efe5
CrezTheHomie <crezyte@gmail.com>
2023-01-12 09:17:28 -0800

    Wrong test URL

--- Client.py
+++ Client.py
@@ -1,17 +1,16 @@
 import requests
 import json
 
-URL = "http://127.0.0.1:5000/predict"
+URL = "http://127.0.0.1:30000/predict"
 TEST_AUDIO_FILE_PATH = "Test\\00f0204f_nohash_0.wav"
 
 
 def make_request_to_bento_service(
     service_url: str, input_file
 ) -> str:
-    serialized_input_data = json.dumps(input_file.tolist())
     response = requests.post(
         service_url,
-        data=serialized_input_data,
+        data=input_file,
         headers={"content-type": "application/json"}
     )
     return response.text
@@ -19,7 +18,8 @@ def make_request_to_bento_service(
 if __name__ == "__main__":
     audio_file = open(TEST_AUDIO_FILE_PATH, "rb")
     values = {"file": (TEST_AUDIO_FILE_PATH, audio_file, "audio/wav")}
-    response = requests.post(url=URL, files=values)
-    data = response.json()
 
-    print(f"Predicted keyword was: {data['keyword']}")
+    response = make_request_to_bento_service(service_url=URL, input_file=values)
+    # data = response.json()
+
+    print(f"Predicted keyword was: {response}")
